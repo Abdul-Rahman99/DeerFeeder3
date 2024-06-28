c13a7c0bfeaeef1c910849de83ef2ed1aef3dcf7
Abdelrahman Ashraf <mr.abdo1213@gmail.com>
2024-06-28 14:09:26 +0400

    Script to fetch the old data and update the db

--- Backend/controllers/dashboardcontroller.js
+++ Backend/controllers/dashboardcontroller.js
@@ -119,14 +119,14 @@ const getFeedPercentage = async (feederId) => {
       }, Remaining feed: ${remainingFeed}, Feed level percentage: ${feedLevelPercentage}%`
     );
 
-    // let query = `UPDATE FeedingDevices
-    //           SET feed_level = ${remainingFeed}, feed_level_percentage = ${feedLevelPercentage}
-    //           WHERE FeedingDevices  .id = ${feederId};
-    //           `;
+    let query = `UPDATE FeedingDevices
+              SET feed_level = ${remainingFeed}, feed_level_percentage = ${feedLevelPercentage}
+              WHERE FeedingDevices  .id = ${feederId};
+              `;
 
-    // await models.sequelize.query(query, {
-    //   type: QueryTypes.UPDATE,
-    // });
+    await models.sequelize.query(query, {
+      type: QueryTypes.UPDATE,
+    });
 
     return feedLevelPercentage;
   } catch (error) {
@@ -135,39 +135,6 @@ const getFeedPercentage = async (feederId) => {
   }
 };
 
-/*
-const getFeedPercentage = async (feederId) => {
-  const tankCapacity = 800; // Assuming a constant tank capacity as you mentioned earlier
-
-  // Fetch feeding done records from 11/6 until now
-  const startDate = new Date("2024-06-11");
-  const endDate = new Date();
-
-  const feedingDoneRecords = await models.FeedingDone.findAll({
-    where: {
-      feeder_id: feederId,
-      createdAt: {
-        [Op.between]: [startDate, endDate],
-      },
-    },
-  });
-
-  let totalFeedUsed = 0;
-
-  // Calculate feed used based on feeding done records
-  feedingDoneRecords.forEach((record) => {
-    const clientMessage = JSON.parse(record.client_message);
-    const milliseconds = parseInt(clientMessage["61"], 10);
-    const seconds = milliseconds / 1000;
-    totalFeedUsed += seconds * 0.4;
-  });
-
-  const adjustedFeedUsed = Math.max(0, totalFeedUsed);
-  const remainingFeed = tankCapacity - adjustedFeedUsed;
-  const feedLevelPercentage = (remainingFeed / tankCapacity) * 100;
-
-  return feedLevelPercentage;
-};*/
 
 const getFeedLevelData = async (req, res) => {
   const loggedInUserId = req.session?.user?.id;
@@ -200,7 +167,8 @@ const getFeedLevelData = async (req, res) => {
     let AllFeedLevels = [];
 
     for (let i = 0; i < records.length; i++) {
-      const { id, title, location, other_info, tankLevel } = records[i];
+      const { id, title, location, other_info, tankLevel } = records[i];  
+      await getFeedPercentage();
 
       const myNewAr = {
         id,
@@ -227,257 +195,6 @@ const getFeedLevelData = async (req, res) => {
     res.status(500).send({ message: error.message });
   }
 };
-
-/*const getFeedPercentage = async (feederId) => {
-  const tankCapacity = 800;
-
-  // Fetch feeding done records for today
-  const today = new Date();
-  const startOfDay = new Date(today.setHours(0, 0, 0, 0));
-  const endOfDay = new Date(today.setHours(23, 59, 59, 999));
-
-  const feedingDoneRecords = await models.FeedingDone.findAll({
-    where: {
-      feeder_id: feederId,
-      createdAt: {
-        [Op.between]: [startOfDay, endOfDay],
-      },
-    },
-  });
-
-  let totalFeedUsed = 0;
-
-  // Calculate feed used based on feeding done records
-  feedingDoneRecords.forEach((record) => {
-    const clientMessage = JSON.parse(record.client_message);
-    const milliseconds = parseInt(clientMessage["61"], 10);
-    const seconds = milliseconds / 1000;
-    totalFeedUsed += seconds * 0.4;
-  });
-
-  const adjustedFeedUsed = Math.max(0, totalFeedUsed);
-  const feedLevelPercentage =
-    ((tankCapacity - adjustedFeedUsed) / tankCapacity) * 100;
-
-  return feedLevelPercentage;
-};
-
-const getFeedLevelData = async (req, res) => {
-  let date_now = new Date();
-  let date_pre = date_now - 518400000; //432000000;
-
-  date_now = new Date(date_now);
-  date_pre = new Date(date_pre);
-
-  var loggedInUserId = req.session?.user?.id;
-  if (!loggedInUserId) {
-    loggedInUserId = "";
-  } else {
-    loggedInUserId = `AND UserDevices.user_id = ${loggedInUserId}`;
-  }
-  const query = `SELECT 
-                        FeedingDevices.id,
-                        FeedingDevices.title, 
-                        FeedingDevices.mac_address, 
-                        FeedingDevices.location, 
-                        FeedingDevices.other_info, 
-                        FeedingDevices.feeder_id 
-                    FROM 
-                        FeedingDevices 
-                    INNER JOIN
-                        UserDevices ON UserDevices.feeder_id = FeedingDevices.id
-                    WHERE 
-                        1=1 ${loggedInUserId}`;
-  const records = await models.sequelize.query(query, {
-    type: QueryTypes.SELECT,
-  });
-
-  let newAr = {};
-  let LowFeedLevels = [];
-  let AllFeedLevels = [];
-  for (let i = 0; i < records.length; i++) {
-    let feederId = records[i].id;
-    let tankLevel = await getFeedPercentage(feederId);
-    let myNewAr = {
-      id: feederId,
-      tankLevel: tankLevel,
-      title: records[i].title,
-      location: records[i].location,
-      other_info: records[i].other_info,
-    };
-    AllFeedLevels.push(myNewAr);
-    if (tankLevel <= 30) {
-      LowFeedLevels.push(feederId);
-    }
-  }
-  newAr = {
-    all: AllFeedLevels,
-    all_c: AllFeedLevels.length,
-    low_c: LowFeedLevels.length,
-  };
-  res.status(200).send(newAr);
-};*/
-//const getFeedPercentage = async(ping_dist) => {
-// return await getFeedDones(feeder_id);
-//let ping_dist_p = (130 - ping_dist) / (1.3)
-//let truncated_ping_dist_p = ping_dist_p.toFixed(0).toString().slice(0, 2);
-// console.log(truncated_ping_dist_p);
-//return 100 - truncated_ping_dist_p;
-
-// if (ping_dist >= 64) {
-//   return 0;
-// } else if (ping_dist >= 55) {
-//   return 20;
-// } else if (ping_dist >= 44) {
-//   return 40;
-// } else if (ping_dist >= 35) {
-//   return 60;
-// } else if (ping_dist >= 20) {
-//   return 80;
-// } else {
-//   return 100;
-// }
-// let total_cm = 70;
-// if (ping_dist > total_cm) {
-//     ping_dist = 70;
-// }
-// return Math.round((total_cm - ping_dist) * 100 / total_cm);
-//};
-
-/*const getFeedLevelData = async (req, res) => {
-    let date_now = new Date();
-    let date_pre = date_now - 518400000; //432000000;
-
-    date_now = new Date(date_now)
-    date_pre = new Date(date_pre)
-
-    var loggedInUserId = req.session?.user?.id;
-    if (!loggedInUserId) {
-        loggedInUserId = ""
-    } else {
-        loggedInUserId = `AND UserDevices.user_id = ${loggedInUserId}`
-    }
-    const query = `SELECT 
-                        FeedingDevices.id,
-                        FeedingDevices.title, 
-                        FeedingDevices.mac_address, 
-                        FeedingDevices.location, 
-                        FeedingDevices.other_info, 
-                        FeedingDevices.feeder_id 
-                    FROM 
-                        FeedingDevices 
-                    INNER JOIN
-                        UserDevices ON UserDevices.feeder_id = FeedingDevices.id
-                    WHERE 
-                        1=1 ${loggedInUserId}`;
-    const records = await models.sequelize.query(query, {
-        type: QueryTypes.SELECT
-    });
-
-    let newAr = {}
-    let LowFeedLevels = []
-    let AllFeedLevels = []
-    for (i = 0; i < records.length; i++) {
-        let feederId = records[i].id;
-        let tankLevel = await getFeedPercentage(feederId);
-        let myNewAr = { id: feederId, tankLevel: tankLevel, title: records[i].title, location: records[i].location, other_info: records[i].other_info }
-        AllFeedLevels.push(myNewAr);
-        if (tankLevel <= 30) {
-         s.push(feederId);
-        }
-    }
-    newAr = { "all": AllFeedLevels, "all_c": AllFeedLevels.length, "low_c": LowFeedLevels.length }
-    res.status(200).send(newAr);
-}*/
-
-/*const getFeedLevelData = async (req, res) => {
-  try {
-    let date_now = new Date();
-    let date_pre = new Date(date_now.getTime() - 518400000);
-
-    let loggedInUserId = req.session?.user?.id;
-    if (loggedInUserId) {
-      loggedInUserId = `AND UserDevices.user_id = :userId`;
-    } else {
-      loggedInUserId = "";
-    }
-
-    const query = `SELECT 
-                        FeedingDevices.id,
-                        FeedingDevices.title, 
-                        FeedingDevices.mac_address, 
-                        FeedingDevices.location, 
-                        FeedingDevices.other_info, 
-                        FeedingDevices.feeder_id
-                    FROM 
-                        FeedingDevices 
-                    INNER JOIN
-                        UserDevices ON UserDevices.feeder_id = FeedingDevices.id
-                    WHERE 
-                        1=1 ${loggedInUserId}`;
-
-    const records = await models.sequelize.query(query, {
-      type: QueryTypes.SELECT,
-      replacements: { userId: req.session?.user?.id },
-    });
-
-    let newAr = {};
-    let LowFeedLevels = [];
-    let AllFeedLevels = [];
-    const initialTankCapacity = 800;
-
-    for (let i = 0; i < records.length; i++) {
-      let feederId = records[i].id;
-
-      // Query to get the total quantity of feed used for performed schedules
-      const scheduleQuery = `
-        SELECT SUM(quantity) as totalFeedUsed
-        FROM FoodSchedules 
-        WHERE 
-          feeder_id = :feederId AND 
-          is_enabled = 1 AND 
-          feed_time <= :currentTime`;
-
-      const result = await models.sequelize.query(scheduleQuery, {
-        type: QueryTypes.SELECT,
-        replacements: {
-          feederId: feederId,
-          currentTime: date_now,
-        },
-      });
-
-      const totalFeedUsed = result[0].totalFeedUsed || 0;
-      const adjustedFeedUsed = Math.max(0, totalFeedUsed * 0.8);
-      const feedLevelPercentage =
-        ((initialTankCapacity - adjustedFeedUsed) / initialTankCapacity) * 100;
-
-      let myNewAr = {
-        id: feederId,
-        tankLevel: feedLevelPercentage,
-        title: records[i].title,
-        location: records[i].location,
-        other_info: records[i].other_info,
-      };
-      AllFeedLevels.push(myNewAr);
-      if (feedLevelPercentage <= 30) {
-        LowFeedLevels.push(feederId);
-      }
-    }
-
-    newAr = {
-      all: AllFeedLevels,
-      all_c: AllFeedLevels.length,
-      low_c: LowFeedLevels.length,
-    };
-    res.status(200).send(newAr);
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .send({ error: "An error occurred while fetching feed level data" });
-  }
-};
-*/
 
 const getFeedLocations = async (req, res) => {
   let data = await models.FeedingDevices.findAll();
