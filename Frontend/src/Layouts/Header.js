cae6308e56407fffb97ebe6d81c9c87f3cd31dba
Abdallah Farag <99741501+Al0olo@users.noreply.github.com>
2024-08-20 13:30:05 +0400

    Update Header.js

--- Frontend/src/Layouts/Header.js
+++ Frontend/src/Layouts/Header.js
@@ -37,26 +37,9 @@ const Header = ({ onChangeLayoutMode, layoutModeType, headerClass }) => {
 
     const toogleMenuBtn = () => {
         var windowSize = document.documentElement.clientWidth;
-        dispatch(changeSidebarVisibility("show"));
-        if (windowSize > 767)
-            document.querySelector(".hamburger-icon").classList.toggle('open');
-        //For collapse horizontal menu
-        if (document.documentElement.getAttribute('data-layout') === "horizontal") {
-            document.body.classList.contains("menu") ? document.body.classList.remove("menu") : document.body.classList.add("menu");
-        }
-        //For collapse vertical and semibox menu
-        if (sidebarVisibilitytype === "show" && (document.documentElement.getAttribute('data-layout') === "vertical" || document.documentElement.getAttribute('data-layout') === "semibox")) {
-            if (windowSize < 1025 && windowSize > 767) {
-                document.body.classList.remove('vertical-sidebar-enable');
-                (document.documentElement.getAttribute('data-sidebar-size') === 'sm') ? document.documentElement.setAttribute('data-sidebar-size', '') : document.documentElement.setAttribute('data-sidebar-size', 'sm');
-            } else if (windowSize > 1025) {
+        // dispatch(changeSidebarVisibility("show"));
                 document.body.classList.remove('vertical-sidebar-enable');
                 (document.documentElement.getAttribute('data-sidebar-size') === 'lg') ? document.documentElement.setAttribute('data-sidebar-size', 'sm') : document.documentElement.setAttribute('data-sidebar-size', 'lg');
-            } else if (windowSize <= 767) {
-                document.body.classList.add('vertical-sidebar-enable');
-                document.documentElement.setAttribute('data-sidebar-size', 'lg');
-            }
-        }
         //Two column menu
         if (document.documentElement.getAttribute('data-layout') === "twocolumn") {
             document.body.classList.contains('twocolumn-panel') ? document.body.classList.remove('twocolumn-panel') : document.body.classList.add('twocolumn-panel');
